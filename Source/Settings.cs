70cd9773333c10bc7eb78cfb943361859c8f2b18
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-04-17 00:14:18 +0900

    add SimpleIniFileParser

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using Extensions;
 
 namespace MasterConverter
 {
@@ -42,8 +44,56 @@ namespace MasterConverter
         {
             var iniFilePath = "./settings.ini";
 
+#if false
             Master = IniFile.Read<MasterSettings>("Master", iniFilePath);
             Export = IniFile.Read<ExportSettings>("Export", iniFilePath);
+#else
+            Master = SimpleIniFileParser.Read<MasterSettings>("Master", iniFilePath);
+            Export = SimpleIniFileParser.Read<ExportSettings>("Export", iniFilePath);
+#endif
         }
+    }
+}
+
+
+public static class SimpleIniFileParser
+{
+    public static T Read<T>(string section, string filepath)
+    {
+        T ret = (T)Activator.CreateInstance(typeof(T));
+        var lines = File.ReadAllLines(filepath);
+
+        foreach (var n in typeof(T).GetFields())
+        {
+            try
+            {
+                if (n.FieldType == typeof(int))
+                {
+                    var x = lines.FirstOrDefault(s => { return s.StartsWith(n.Name); });
+                    n.SetValue(ret, int.Parse(x.Split('=')[1].Trim()));
+                }
+                else if (n.FieldType == typeof(uint))
+                {
+                    var x = lines.FirstOrDefault(s => { return s.StartsWith(n.Name); });
+                    n.SetValue(ret, uint.Parse(x.Split('=')[1].Trim()));
+                }
+                else if (n.FieldType == typeof(bool))
+                {
+                    var x = lines.FirstOrDefault(s => { return s.StartsWith(n.Name); });
+                    n.SetValue(ret, bool.Parse(x.Split('=')[1].Trim()));
+                }
+                else
+                {
+                    var x = lines.FirstOrDefault(s => { return s.StartsWith(n.Name); });
+                    n.SetValue(ret, x.Split('=')[1].Trim());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        };
+
+        return ret;
     }
 }
